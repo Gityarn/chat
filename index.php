1cd56d2f6081664e5cd38ff7272d7948caab567d
Gityarn <96947885+Gityarn@users.noreply.github.com>
2022-01-23 21:03:31 +0800

    add function

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
-	$operation=trim($_POST["operation"]);//获取操作类型 ls聊天室列表 new创建聊天室 del删除聊天室
+    //聊天室列表
+    $dir=scandir("./");
+    $dir = array_diff($dir, [".", "..","default","index.php"]);//屏蔽非聊天室目录
+    foreach ($dir as $value)
+    {
+        if(is_dir($value) and !strpos($value,'_hidden'))//屏蔽文件、有隐藏标记目录
+        {
+            echo "<a href=\"./".$value."/index.php\">".$value."</a><br/>";
+        }
+    }
+
+//处理post请求
+if($_POST){
+    $operation=trim($_POST["operation"]);//获取操作类型 ls聊天室列表 new创建聊天室 del删除聊天室
 	$name=trim($_POST["name"]);//获取待创建的聊天室名称name
-	echo $_POST["operation"];
- if ($operation=="ls") {
-     $dir=scandir("./");
-     $dir = array_diff($dir, [".", "..","default","index.php"]);//屏蔽非聊天室目录
-     foreach ($dir as $value)
-     {
-         if(is_dir($value) and !strpos($value,'_hidden'))//屏蔽文件、有隐藏标记目录
-         {
-             //echo $value . PHP_EOL;
-             echo $value . "<br>";
-         }
-     }
+	//echo $_POST["operation"];
+if ($operation=="ls") {
+
  }
  elseif ($operation=="new" and $name!=null)
  {
@@ -26,10 +30,31 @@
          copy("./default/write.php",$dir."/write.php");
      }
      else {
-         echo "该名称已存在";
+         echo "<font color=\"#FF0000\">创建聊天室失败：该名称已存在！</font>";
      }
      
  }
      //print_r($dir);
 
- ?>
+} ?>
+ 
+ <!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8"> 
+<title>普鲁士</title> 
+</head>
+<body>
+
+<form action="" method="post">
+聊天室名称: <input type="text" name="name">
+<input type="submit" value="new" name="operation">
+</form>
+
+<form action="" method="post">
+聊天室名称: <input type="text" name="name">
+<input type="submit" value="del" name="operation">
+</form>
+
+</body>
+</html>
